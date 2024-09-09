19b4037be3e3f5ed5a3f58303b9259246f53d53c
purebver <wkddj1541@gmail.com>
2024-09-09 12:54:24 +0900

    아이템 완료, 토큰 수정

--- src/routes/item.router.js
+++ src/routes/item.router.js
@@ -34,4 +34,66 @@ router.post('/item-create', async (req, res, next) => {
   }
 });
 
+router.patch('/item-renewal/:itemId', async (req, res, next) => {
+  try {
+    const { itemId } = req.params;
+    const updatedData = req.body;
+
+    delete updatedData.price;
+
+    const item = await prisma.items.findFirst({
+      where: { itemId: +itemId },
+    });
+
+    await prisma.$transaction(async (tx) => {
+      await tx.items.update({
+        data: { ...updatedData },
+        where: { itemId: +itemId },
+      });
+    });
+
+    const renewal_data = {};
+    for (let key in updatedData) {
+      if (item[key] !== updatedData[key]) {
+        renewal_data[`old${key}`] = item[key];
+        renewal_data[`new${key}`] = updatedData[key];
+      }
+    }
+
+    return res.status(200).json({
+      renewal_data,
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/item', async (req, res, next) => {
+  const items = await prisma.items.findMany({
+    select: {
+      itemId: true,
+      name: true,
+      price: true,
+    },
+    orderBy: {
+      itemId: 'asc',
+    },
+  });
+  return res.status(200).json({ items });
+});
+
+router.get('/item/:itemId', async (req, res, next) => {
+  const { itemId } = req.params;
+  const item = await prisma.items.findFirst({
+    where: { itemId: +itemId },
+    select: {
+      itemId: true,
+      name: true,
+      baseState: true,
+      price: true,
+    },
+  });
+  return res.status(200).json({ item });
+});
+
 export default router;
