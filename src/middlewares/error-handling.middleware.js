72859be7ede1b8b300df91a534c975837a9ee6c4
purebver <wkddj1541@gmail.com>
2024-09-10 21:26:49 +0900

    Comment: 주석 추가

--- src/middlewares/error-handling.middleware.js
+++ src/middlewares/error-handling.middleware.js
@@ -1,18 +1,25 @@
 export default function (err, req, res, next) {
   console.log(err);
+  //prisma에서 create 시 유니크값이 존재할경우
   if (err.code === 'P2002') {
     return res
       .status(409)
       .json({ message: '이미 존재하는 아이디(이름) 입니다.' });
   }
+
+  //주로 prisma에서 검색한 대상이 undefined일 경우 발생
   if (err.code === undefined) {
     return res.status(404).json({
       message: '선택한 대상을 찾을 수 없습니다.',
     });
   }
+
+  //주로 undefined로 prisma에서 검색하면 발생
   if (err.code === 'P2025') {
     return res.status(404).json({ message: '선택한 대상을 찾을 수 없습니다.' });
   }
+
+  //그 외 에러에 대한 응답
   return res.status(500).json({
     message: '서버 내부에서 에러가 발생했습니다.',
   });
--- src/middlewares/log.middleware.js
+++ src/middlewares/log.middleware.js
@@ -1,16 +1,28 @@
 import winston from 'winston';
 
+//로깅 설정
 const logger = winston.createLogger({
+  //로그레벨(중요도)을 설정 정보를 얼마나 상세하게 받을지 지정
   level: 'info',
+
+  //로그를 json형태로 출력
   format: winston.format.json(),
+
+  //로그를 콘솔에 출력
   transports: [new winston.transports.Console()],
 });
 
+//미들웨어
 export default function (req, res, next) {
+  //요청이 시작된 시점을 기록
   const start = new Date().getTime();
 
+  //응답이 끝났을 경우(finish)
   res.on('finish', () => {
+    //시작된 시점을 이용해서 응답이 완료될 때까지 걸린 시간을 계산
     const duration = new Date().getTime() - start;
+
+    //작성될 로그의 형태
     logger.info(
       `Method: ${req.method}, URL: ${req.url}, Status: ${res.statusCode}, Duration: ${duration}ms`
     );
